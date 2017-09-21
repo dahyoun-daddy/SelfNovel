e1b7b6f5c85717a8ab6197e6d9155cfe3840761c
zpqls1536 <zpqls1536@naver.com>
2017-09-21 14:33:08 +0900

    최초작성

---
+++ SelfNovel/src/main/java/com/sn/img/dao/ImgDaoImpl.java
@@ -0,0 +1,59 @@
+package com.sn.img.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sn.common.DTO;
+import com.sn.img.domain.ImgVO;
+
+public class ImgDaoImpl implements ImgDao {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	private final String namespace = "com.sn.codes.repository.mappers.codes";
+	
+	@Override
+	public int do_save(DTO dto) {
+		String statement = namespace +".do_save";
+		
+		log.debug("===========in do_save===========");
+		log.debug("statement: "+statement);
+		log.debug("dto: "+dto.toString());
+		log.debug("===========// do_save===========");
+		
+		ImgVO imgVO = (ImgVO)dto;
+		return sqlSession.insert(statement, imgVO);
+	}
+
+	@Override
+	public List<?> do_search(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int do_delete(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int do_update(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DTO do_searchOne(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- SelfNovel/src/main/java/com/sn/img/domain/ImgVO.java
+++ SelfNovel/src/main/java/com/sn/img/domain/ImgVO.java
@@ -2,6 +2,96 @@ package com.sn.img.domain;
 
 import com.sn.common.DTO;
 
+/**
+ * ImgVO
+ * 
+ * 최초작성: 2017-09-21
+ * @author sist
+ *
+ */
 public class ImgVO extends DTO {
+	private int img_id;     //이미지ID
+	private int img_num;    //이미지 순서
+	private String img_org_nm; //원본 파일 이름
+	private String img_sv_nm;  //저장 파일 이름
+	private String img_path;   //파일 경로
+	private int img_use_yn; //사용여부
+	
+	/***********************************************/
+	//constructor
+	/***********************************************/
+	public ImgVO() {
+		
+	}
+	
+	public ImgVO(int img_id, int img_num, String img_org_nm, String img_sv_nm, String img_path, int img_use_yn) {
+		super();
+		this.img_id = img_id;
+		this.img_num = img_num;
+		this.img_org_nm = img_org_nm;
+		this.img_sv_nm = img_sv_nm;
+		this.img_path = img_path;
+		this.img_use_yn = img_use_yn;
+	}
 
+	/***********************************************/
+	//getter and setter
+	/***********************************************/
+	public int getImg_id() {
+		return img_id;
+	}
+
+	public void setImg_id(int img_id) {
+		this.img_id = img_id;
+	}
+
+	public int getImg_num() {
+		return img_num;
+	}
+
+	public void setImg_num(int img_num) {
+		this.img_num = img_num;
+	}
+
+	public String getImg_org_nm() {
+		return img_org_nm;
+	}
+
+	public void setImg_org_nm(String img_org_nm) {
+		this.img_org_nm = img_org_nm;
+	}
+
+	public String getImg_sv_nm() {
+		return img_sv_nm;
+	}
+
+	public void setImg_sv_nm(String img_sv_nm) {
+		this.img_sv_nm = img_sv_nm;
+	}
+
+	public String getImg_path() {
+		return img_path;
+	}
+
+	public void setImg_path(String img_path) {
+		this.img_path = img_path;
+	}
+
+	public int getImg_use_yn() {
+		return img_use_yn;
+	}
+
+	public void setImg_use_yn(int img_use_yn) {
+		this.img_use_yn = img_use_yn;
+	}
+
+	/***********************************************/
+	//method
+	/***********************************************/
+	@Override
+	public String toString() {
+		return "ImgVO [img_id=" + img_id + ", img_num=" + img_num + ", img_org_nm=" + img_org_nm + ", img_sv_nm="
+				+ img_sv_nm + ", img_path=" + img_path + ", img_use_yn=" + img_use_yn + "]";
+	}
+	
 }
